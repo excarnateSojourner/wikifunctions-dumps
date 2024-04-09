0331cc6d9aee97cb8b0e0eaa66b7ffb4035bedae
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-04-09 16:30:55 -0700

    lacking_lang: Broaden purpose of lacking_label
    Broaden the purpose of lacking_label.py and enable it to
    find objects lacking descriptions and aliases as well.

--- lacking_lang.py
+++ lacking_lang.py
@@ -2,10 +2,13 @@ import argparse
 import collections
 import json
 
+TYPE_STRING = 'Z6'
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('input_path')
 	parser.add_argument('output_path_prefix')
+	parser.add_argument('-p', '--property', default='label')
 	args = parser.parse_args()
 
 	with open(args.input_path, encoding='utf-8') as in_file:
@@ -13,8 +16,12 @@ def main():
 
 	lackers = collections.defaultdict(list)
 	for id_, obj in objects.items():
-		if not obj['label']:
-			lackers[obj['value']['Z1K1']].append(id_)
+		if not obj[args.property]:
+			try:
+				lackers[obj['value']['Z1K1']].append(id_)
+			# The value of a String is just the string itself (not a dict)
+			except TypeError:
+				lackers[TYPE_STRING].append(id_)
 
 	for type_, ids in lackers.items():
 		with open(f'{args.output_path_prefix}{type_}.wiki', 'w', encoding='utf-8') as out_file:
