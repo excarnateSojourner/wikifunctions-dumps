cc1cd4f53f0eece9f5f5df0771c36c7a05a9431b
excarnateSojourner <89501119+excarnateSojourner@users.noreply.github.com>
2024-04-11 12:14:28 -0700

    parse_types: Call Z codes codes, not IDs

--- parse_types.py
+++ parse_types.py
@@ -12,17 +12,17 @@ def main():
 	with open(args.objects_file, encoding='utf-8') as in_file:
 		objects = json.load(in_file)
 
-	ids_to_names = {}
+	codes_to_names = {}
 	for id_, obj in objects.items():
 		try:
 			if obj['value']['Z1K1'] == TYPE_TYPE:
-				ids_to_names[id_] = obj['label']
+				codes_to_names[id_] = obj['label']
 		# Pesky Strings
 		except TypeError:
 			pass
 
-	names_to_ids = {v: k for k, v in ids_to_names.items()}
-	types = {'ids to names': ids_to_names, 'names to ids': names_to_ids}
+	names_to_codes = {v: k for k, v in codes_to_names.items()}
+	types = {'codes to names': codes_to_names, 'names to codes': names_to_codes}
 
 	with open(args.output_file, 'w', encoding='utf-8') as out_file:
 		json.dump(types, out_file, indent='\t')
